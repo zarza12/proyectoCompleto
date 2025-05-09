0faa57ba8aac389a17e5833a1208ce025f574865
Zarza12 <sbraya88@gmail.com>
2025-05-09 00:39:31 -0600

    a4

--- views/produccion/modificarProduccion.php
+++ views/produccion/modificarProduccion.php
@@ -322,7 +322,7 @@ $sectoresJSON = json_encode($listarSectores);
         const modificar = document.getElementById('sectorModificar');
         sectores.forEach(opt => {
             const o1 = document.createElement('option');
-            o1.value = opt.label;
+            o1.value = opt.value;
             o1.textContent = opt.value;
             filtro.appendChild(o1);
             const o2 = o1.cloneNode(true);
